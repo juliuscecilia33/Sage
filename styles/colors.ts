d570ad867d2eccb864b6eedbde90b99f966d95a6
Julius <juliuscecilia33@gmail.com>
2024-05-16 08:15:36 -0700

    color fixes

---
+++ styles/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  primary: {
+    text: "text-primary",
+    bg: "bg-primary",
+  },
+  secondary: {
+    text: "text-secondary",
+    bg: "bg-secondary",
+  },
+  accentPrimary: {
+    text: "text-accentPrimary",
+  },
+  accentSecondary: {
+    text: "text-accentSecondary",
+  },
+  // Add more color variables as needed
+} as const;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "#000000", // Text color
+          bg: "#FFFFFF", // Background color
+        },
+        secondary: {
+          DEFAULT: "#000000", // Text color
+          bg: "#FBFCFD", // Background color
+        },
+        accentPrimary: {
+          DEFAULT: "#956E60", // Text color, Brown
+        },
+        accentSecondary: {
+          DEFAULT: "#B5B5B5", // Text color, Gray
+        },
+        background: "#FFFFFF",
+        foreground: "#FBFCFD",
         btn: {
-          background: "hsl(var(--btn-background))",
+          background: "#FEF2EE",
           "background-hover": "hsl(var(--btn-background-hover))",
         },
       },
