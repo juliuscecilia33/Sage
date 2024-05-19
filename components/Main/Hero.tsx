f3bb24f70c2e2e5d5e2271eda0e5740811924628
Julius <juliuscecilia33@gmail.com>
2024-05-18 21:21:39 -0700

    started action tool bar

--- components/Main/Hero.tsx
+++ components/Main/Hero.tsx
@@ -1,5 +1,7 @@
 import { colors } from "../../styles/colors";
 import { FaChevronDown } from "react-icons/fa";
+import { IoMdUndo } from "react-icons/io";
+import { IoMdRedo } from "react-icons/io";
 
 type HeroProps = {
   bookTitle: string;
@@ -10,7 +12,7 @@ const Hero = ({ bookTitle, chapterCount }: HeroProps) => {
   return (
     <div className="flex flex-col mt-5">
       <div className="flex flex-row w-full justify-between items-center">
-        <h2 className="font-semibold text-2xl text-black ">{bookTitle}</h2>
+        <h2 className="font-semibold text-3xl text-black ">{bookTitle}</h2>
         <div className="flex flex-row items-center">
           <button
             className={`flex justify-center items-center px-4 mr-3 py-1.5 text-sm font-inter font-medium rounded-sm font-archivo ${colors.button.secondary}`}
@@ -23,6 +25,16 @@ const Hero = ({ bookTitle, chapterCount }: HeroProps) => {
           >
             Chapter {chapterCount}
             <FaChevronDown className="ml-1" />
+          </button>
+        </div>
+      </div>
+      <div className="mt-5 flex flex-row w-full justify-between items-center">
+        <div className="flex flex-row items-center">
+          <button className="text-[#956E60] mr-5">
+            <IoMdUndo size={20} />
+          </button>
+          <button className="text-[#B5B5B5]">
+            <IoMdRedo size={20} />
           </button>
         </div>
       </div>
