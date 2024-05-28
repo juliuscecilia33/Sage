a8240bde56b6bca3205ef870be82165dae014b16
Julius <juliuscecilia33@gmail.com>
2024-05-27 18:09:15 -0700

    added authentication

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -16,7 +16,7 @@ export default function AuthForm() {
       view="magic_link"
       showLinks={false}
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo={`${process.env.DEV_URL}/auth/callback`}
       appearance={{ theme: ThemeSupa }}
     />
   );
--- middleware.ts
+++ middleware.ts
@@ -21,5 +21,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/home"], // chanfge back to dashboard
+  matcher: ["/", "/dashboard"], // chanfge back to dashboard
 };
