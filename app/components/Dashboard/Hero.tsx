ff431ad8415797727e2402a4c071ae853c07034b
Julius <juliuscecilia33@gmail.com>
2024-05-31 14:48:43 -0700

    changed fonts

--- app/components/Dashboard/Hero.tsx
+++ app/components/Dashboard/Hero.tsx
@@ -18,21 +18,23 @@ type HeroProps = {
 const Hero = ({ bookTitle, chapterCount }: HeroProps) => {
   return (
     <div
-      className={`flex flex-col mt-4 pb-4 px-4 border-b border-[#F0F0F0] ${paragraphFont.className}`}
+      className={`${paragraphFont.className} flex flex-col mt-4 pb-4 px-4 border-b border-[#F0F0F0]`}
     >
       <div className="flex flex-row w-full justify-between items-center">
-        <h2 className="font-semibold text-3xl text-black font-archivo">
+        <h2
+          className={`${titleFont.className} font-semibold text-3xl text-black font-archivo`}
+        >
           {bookTitle}
         </h2>
         <div className="flex flex-row items-center">
           <button
-            className={`flex justify-center items-center px-4 mr-3 py-1.5 text-sm font-inter font-medium rounded-sm font-archivo ${colors.button.secondary}`}
+            className={`${paragraphFont.className} flex justify-center items-center px-4 mr-3 py-1.5 text-sm font-medium rounded-sm font-archivo ${colors.button.secondary}`}
           >
             ESV
             <FaChevronDown className="ml-1" />
           </button>
           <button
-            className={`flex justify-center items-center px-4 py-1.5 text-sm font-inter font-medium rounded-sm font-archivo ${colors.button.secondary}`}
+            className={`${paragraphFont.className} flex justify-center items-center px-4 py-1.5 text-sm font-medium rounded-sm font-archivo ${colors.button.secondary}`}
           >
             Chapter {chapterCount}
             <FaChevronDown className="ml-1" />
@@ -47,23 +49,27 @@ const Hero = ({ bookTitle, chapterCount }: HeroProps) => {
           <button className="text-[#B5B5B5] pr-5 border-r border-[#F0F0F0]">
             <IoMdRedo size={20} />
           </button>
-          <button className="flex flex-row items-center font-medium px-5 border-r border-[#F0F0F0] font-archivo text-[#956E60]">
+          <button
+            className={`${paragraphFont.className} flex flex-row items-center font-medium px-5 border-r border-[#F0F0F0] font-archivo text-[#956E60]`}
+          >
             <p>Helvetica</p>
             <FaChevronDown className="ml-2" />
           </button>
-          <button className="flex flex-row items-center font-medium px-5 border-r border-[#F0F0F0] font-archivo text-[#956E60]">
+          <button
+            className={`${paragraphFont.className} flex flex-row items-center font-medium px-5 border-r border-[#F0F0F0] font-archivo text-[#956E60]`}
+          >
             <MdStickyNote2 className="mr-2" size={28} />
             <p>Sticky</p>
           </button>
         </div>
         <div className="flex flex-row items-center">
           <button
-            className={`mr-3 border border-[#956E60] flex justify-center items-center p-1.5 text-sm font-inter font-medium rounded font-archivo bg-[#FEF2EE] text-[#956E60]`}
+            className={`mr-3 border border-[#956E60] flex justify-center items-center p-1.5 text-sm font-medium rounded font-archivo bg-[#FEF2EE] text-[#956E60]`}
           >
             <FaBookBible size={20} />
           </button>
           <button
-            className={`transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] border border-[#B5B5B5] flex justify-center items-center p-1.5 text-sm font-inter font-medium rounded font-archivo bg-[#FBFCFD] text-[#B5B5B5]`}
+            className={`transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] border border-[#B5B5B5] flex justify-center items-center p-1.5 text-sm font-medium rounded font-archivo bg-[#FBFCFD] text-[#B5B5B5]`}
           >
             <CgNotes size={20} />
           </button>
--- app/components/Dashboard/Navbar.tsx
+++ app/components/Dashboard/Navbar.tsx
@@ -1,23 +1,24 @@
 import { FaChevronRight, FaChevronLeft, FaChevronDown } from "react-icons/fa";
 import { colors } from "../../../utils/colors";
+import { paragraphFont, titleFont } from "@/utils/fonts";
 
 const Navbar = () => {
   return (
     <div className="px-4 pt-4 flex flex-row w-full justify-between items-center">
       <button
-        className={`flex justify-center items-center px-4 py-1.5 text-sm font-inter font-medium rounded-sm font-archivo bg-[#FEF2EE] text-[#956E60]`}
+        className={`${paragraphFont.className} flex justify-center items-center px-4 py-1.5 text-sm font-inter font-medium rounded-sm font-archivo bg-[#FEF2EE] text-[#956E60]`}
       >
         <FaChevronLeft className="mr-1" />
         Revelation
       </button>
       <button
-        className={`flex justify-center items-center px-4 py-1.5 text-sm font-inter font-medium rounded-sm font-archivo bg-[#FBFCFD] text-[#B5B5B5]`}
+        className={`${paragraphFont.className} flex justify-center items-center px-4 py-1.5 text-sm font-inter font-medium rounded-sm font-archivo bg-[#FBFCFD] text-[#B5B5B5]`}
       >
         Genesis
         <FaChevronDown className="ml-1" />
       </button>
       <button
-        className={`flex justify-center items-center px-4 py-1.5 text-sm font-inter font-medium rounded-sm font-archivo bg-[#FEF2EE] text-[#956E60]`}
+        className={`${paragraphFont.className} flex justify-center items-center px-4 py-1.5 text-sm font-inter font-medium rounded-sm font-archivo bg-[#FEF2EE] text-[#956E60]`}
       >
         Exodus
         <FaChevronRight className="ml-1" />
