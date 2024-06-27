b888489b52f2b555490c624453c5d7890f2233d6
Julius <juliuscecilia33@gmail.com>
2024-06-27 16:13:42 -0700

    Update Modal.tsx

--- app/components/SideNotes/Modal.tsx
+++ app/components/SideNotes/Modal.tsx
@@ -13,7 +13,7 @@ const Modal = ({ show, onClose }: ModalProps) => {
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-[#F9F9FA] rounded-lg overflow-hidden shadow-xl transform transition-all w-1/2 p-10">
+      <div className="bg-[#F9F9FA] rounded-lg overflow-hidden shadow-xl transform transition-all w-3/5 p-16">
         <div className="flex justify-between items-center">
           <div className={`flex items-center`}>
             <h3 className="text-3xl font-bold text-[#11181C]">Create a Note</h3>
@@ -30,7 +30,7 @@ const Modal = ({ show, onClose }: ModalProps) => {
             <MdClose size={30} />
           </button>
         </div>
-        <div className="flex flex-col rounded mt-4 mb-4 bg-white w-full px-10 pt-6 pb-10">
+        <div className="flex flex-col rounded mt-4 mb-8 bg-white w-full px-6 pt-6 pb-10">
           <input
             className="mb-2 p-4 font-bold text-3xl text-black border-none outline-none focus:ring-0 placeholder:text-3xl placeholder:font-bold"
             id="title"
@@ -48,42 +48,52 @@ const Modal = ({ show, onClose }: ModalProps) => {
           />
         </div>
         <div className="flex items-center mb-8">
-          <div className="flex rounded bg-white px-4 py-3 mr-4 items-center">
-            <div className="w-10 h-10 bg-blue-500 rounded-full mr-4"></div>
-            <div className="w-10 h-10 bg-blue-500 rounded-full mr-4"></div>
-            <div className="w-10 h-10 bg-blue-500 rounded-full mr-4"></div>
-            <div className="w-10 h-10 bg-blue-500 rounded-full mr-4"></div>
-            <div className="w-10 h-10 bg-blue-500 rounded-full mr-4"></div>
-            <div className="w-10 h-10 bg-blue-500 rounded-full"></div>
+          <div className="flex flex-col">
+            <p className="text-[#B5B5B5] font-light mb-2">
+              Add to your Themes:
+            </p>
+            <div className="flex rounded bg-white px-2 py-3 mr-4 items-center">
+              <button
+                type="button"
+                className={`${paragraphFont.className} mr-3 transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] flex justify-center items-center h-10 px-3 text-sm font-medium rounded bg-[#FBFCFD] text-[#B5B5B5]`}
+              >
+                <div className="w-7 h-7 bg-blue-500 rounded mr-4"></div>
+                Life And Growth
+                <FaChevronDown className="ml-2" />
+              </button>
+            </div>
           </div>
-          <div className="flex rounded bg-white px-3 py-3">
-            <button
-              type="button"
-              className={`${paragraphFont.className} mr-3 transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] flex justify-center items-center h-10 px-6 text-sm font-medium rounded bg-[#FBFCFD] text-[#B5B5B5]`}
-            >
-              Private
-              <FaChevronDown className="ml-2" />
-            </button>
-            <button
-              type="button"
-              className={`${paragraphFont.className} transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] flex justify-center items-center h-10 px-6 text-sm font-medium rounded bg-[#FBFCFD] text-[#B5B5B5]`}
-            >
-              Add To
-              <FaChevronDown className="ml-2" />
-            </button>
+          <div className="flex flex-col">
+            <p className="text-[#B5B5B5] font-light mb-2">Options:</p>
+            <div className="flex rounded bg-white px-3 py-3">
+              <button
+                type="button"
+                className={`${paragraphFont.className} mr-3 transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] flex justify-center items-center h-10 px-6 text-sm font-medium rounded bg-[#FBFCFD] text-[#B5B5B5]`}
+              >
+                Private
+                <FaChevronDown className="ml-2" />
+              </button>
+              <button
+                type="button"
+                className={`${paragraphFont.className} transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] flex justify-center items-center h-10 px-6 text-sm font-medium rounded bg-[#FBFCFD] text-[#B5B5B5]`}
+              >
+                Add To
+                <FaChevronDown className="ml-2" />
+              </button>
+            </div>
           </div>
         </div>
         <div className="w-full flex justify-between items-center">
           <button
             type="button"
             onClick={onClose}
-            className="transition mr-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200"
+            className="transition mr-2 inline-flex justify-center py-3 px-7 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200"
           >
             Cancel
           </button>
           <button
             type="submit"
-            className="transition inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-[#956E60] hover:bg-[#7F5C4F]"
+            className="transition inline-flex justify-center py-3 px-7 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-[#956E60] hover:bg-[#7F5C4F]"
           >
             Add Note
           </button>
