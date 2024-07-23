120c4b454a5105b421efdc8bf828d66e321d5544
Julius <juliuscecilia33@gmail.com>
2024-07-22 17:51:38 -0700

    creating themes modal

--- app/components/Dashboard/Left/Main.tsx
+++ app/components/Dashboard/Left/Main.tsx
@@ -2,38 +2,57 @@ import React, { useState, useEffect } from "react";
 
 import { paragraphFont, titleFont } from "@/utils/fonts";
 import { logout } from "@/app/logout/actions";
+import ThemesModal from "../../Themes/Modal";
 
 const LeftSection = () => {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
 
   const handleSearch = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
   };
 
   return (
-    <div className="w-[18%] h-full py-4 px-3">
-      <form
-        className={`${paragraphFont.className} border-none outline-none w-full mb-5 transition flex justify-between items-center px-4 py-1.5 text-sm font-medium rounded bg-[#FBFCFD] text-[#B5B5B5]`}
-        onSubmit={(e: any) => handleSearch(e)}
-      >
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Type Note, Verse"
-          className="border-none outline-none"
-        />
-        <button type="submit">Search</button>
-      </form>
-      <form action={() => logout()} method="post">
-        <button
-          className={`${paragraphFont.className} w-full transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] border border-[#FEF2EE] flex justify-between items-center px-4 py-1.5 text-sm font-medium rounded bg-[#FEF2EE] text-[#956E60]`}
-          type="submit"
+    <div className="w-[18%] h-full py-4">
+      <div className="w-full border-b border-[#F0F0F0] pb-2 px-3">
+        <form
+          className={`${paragraphFont.className} outline-none w-full mb-5 transition flex justify-between items-center px-4 py-1.5 text-sm font-medium rounded bg-[#FBFCFD] text-[#B5B5B5]`}
+          onSubmit={(e: any) => handleSearch(e)}
         >
-          Sign Out
-        </button>
-      </form>
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Type Note, Verse"
+            className="border-none outline-none p-2"
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+
+      <div className="flex flex-col w-full px-3">
+        <div className="w-full flex justify-between">
+          <p className="text-[#B5B5B5] text-base text-left font-light my-4">
+            Themes Key
+          </p>
+          <button onClick={openModal} className="text-[#956E60] text-base">
+            Add
+          </button>
+          <ThemesModal show={showModal} onClose={closeModal} />
+        </div>
+        <form action={() => logout()} method="post">
+          <button
+            className={`${paragraphFont.className} w-full transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] border border-[#FEF2EE] flex justify-between items-center px-4 py-1.5 text-sm font-medium rounded bg-[#FEF2EE] text-[#956E60]`}
+            type="submit"
+          >
+            Sign Out
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
---
+++ app/components/Themes/Modal.tsx
@@ -0,0 +1,94 @@
+import { paragraphFont } from "@/utils/fonts";
+import React, { useRef } from "react";
+import { MdClose } from "react-icons/md";
+
+interface ModalProps {
+  onClose: () => void;
+  show: boolean;
+}
+
+const ThemesModal = ({ onClose, show }: ModalProps) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const themes = [
+    {
+      count: 26,
+      name: "Light",
+    },
+    {
+      count: 2,
+      name: "Dark",
+    },
+    {
+      count: 22,
+      name: "Brown",
+    },
+    {
+      count: 1,
+      name: "Black",
+    },
+    {
+      count: 23,
+      name: "Yelow",
+    },
+  ];
+
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
+    }
+  };
+
+  if (!show) return null;
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
+      <div className="mr-5 bg-[#F9F9FA] rounded-lg overflow-hidden shadow-xl transform transition-all w-3/5 p-16">
+        <div className="flex justify-between items-center">
+          <div className={`flex items-center`}>
+            <h3 className="text-3xl font-bold text-[#11181C]">Your Themes</h3>
+          </div>
+          <button
+            onClick={onClose}
+            className="transition text-gray-400 hover:text-gray-600"
+          >
+            <MdClose size={30} />
+          </button>
+        </div>
+        <div className="my-10 w-full bg-white flex p-10">
+          {themes.map((theme, key) => (
+            <div className="flex flex-col items-center w-[20%] mr-5">
+              <div className="flex flex-col bg-[#B7C467] p-4 rounded w-full">
+                <div className="w-full flex justify-end mb-20">
+                  <div className="w-10 h-10 aspect-square rounded-full flex justify-center items-center bg-white text-[#B7C467] font-bold text-sm">
+                    {theme.count}
+                  </div>
+                </div>
+                <h3 className="text-xl font-bold text-white">{theme.name}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="w-full flex justify-between items-center">
+          <button
+            type="button"
+            onClick={onClose}
+            className="transition mr-2 inline-flex justify-center py-3 px-7 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={() => {}}
+            className="transition inline-flex justify-center py-3 px-7 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-[#956E60] hover:bg-[#7F5C4F]"
+          >
+            Create Theme
+          </button>
+        </div>
+      </div>
+      <div className="bg-[#F9F9FA] rounded-lg overflow-hidden shadow-xl transform transition-all w-1/5 p-10">
+        <h3 className="text-xl font-bold text-[#11181C]">Community Themes</h3>
+      </div>
+    </div>
+  );
+};
+
+export default ThemesModal;
