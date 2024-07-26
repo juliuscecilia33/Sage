53bb69eaf27673c431842b83af6307a5f5bb2870
Julius <juliuscecilia33@gmail.com>
2024-07-26 13:34:26 -0700

    created theme template

--- app/components/Dashboard/Left/Main.tsx
+++ app/components/Dashboard/Left/Main.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import { paragraphFont, titleFont } from "@/utils/fonts";
 import { logout } from "@/app/logout/actions";
 import ThemesModal from "../../Themes/Modal";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { FaRegEdit } from "react-icons/fa";
 
 const LeftSection = () => {
   const [query, setQuery] = useState("");
@@ -44,9 +46,41 @@ const LeftSection = () => {
           </button>
           <ThemesModal show={showModal} onClose={closeModal} />
         </div>
+        <div className="mt-3 mb-1 w-full flex justify-between bg-[#FBFCFD] p-3">
+          <div className="w-[83%] flex justify-start items-center text-sm">
+            <div className="w-6 h-6 rounded bg-[#B7C467] mr-3"></div>
+            <h3 className="truncate text-[#B7C467] text-medium">
+              Life and Growth
+            </h3>
+          </div>
+          <div className="w-[17%] flex justify-between items-center">
+            <button>
+              <FaRegEdit className="text-[#B5B5B5]" size={15} />
+            </button>
+            <button>
+              <FaRegTrashCan className="text-[#B5B5B5]" size={15} />
+            </button>
+          </div>
+        </div>
+        <div className="w-full flex justify-between p-3">
+          <div className="w-[83%] flex justify-start items-center text-sm">
+            <div className="w-6 h-6 rounded bg-[#B7C467] mr-3"></div>
+            <h3 className="truncate text-[#B7C467] text-medium">
+              Life and Growth
+            </h3>
+          </div>
+          <div className="w-[17%] flex justify-between items-center">
+            <button>
+              <FaRegEdit className="text-[#B5B5B5]" size={15} />
+            </button>
+            <button>
+              <FaRegTrashCan className="text-[#B5B5B5]" size={15} />
+            </button>
+          </div>
+        </div>
         <form action={() => logout()} method="post">
           <button
-            className={`${paragraphFont.className} w-full transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] border border-[#FEF2EE] flex justify-between items-center px-4 py-1.5 text-sm font-medium rounded bg-[#FEF2EE] text-[#956E60]`}
+            className={`mt-10 ${paragraphFont.className} w-full transition hover:border-[#956E60] hover:bg-[#FEF2EE] hover:text-[#956E60] border border-[#FEF2EE] flex justify-between items-center px-4 py-1.5 text-sm font-medium rounded bg-[#FEF2EE] text-[#956E60]`}
             type="submit"
           >
             Sign Out
