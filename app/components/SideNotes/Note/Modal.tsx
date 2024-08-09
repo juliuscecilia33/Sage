a3629f5ea7dddad353963ae42961b3822ed6cfad
Julius <juliuscecilia33@gmail.com>
2024-08-08 23:16:35 -0700

    fixed note modal

---
+++ app/components/SideNotes/Note/Modal.tsx
@@ -0,0 +1,65 @@
+import { useState, Dispatch, SetStateAction } from "react";
+import { MdClose } from "react-icons/md";
+import { paragraphFont } from "@/lib/utils/fonts";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { useCurrentBookDataContext } from "@/app/context/CurrentBookData";
+import { postSideNotesBook } from "@/lib/utils/data/sideNotes/book/postSideNotesBook";
+import { editSideNotesBook } from "@/lib/utils/data/sideNotes/book/editSideNotesBook";
+
+interface SideNote {
+  id: string;
+  title: string;
+  description: string;
+  book: string;
+  verse: string;
+  userTheme: string;
+  isPrivate: boolean;
+  workspaceId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface ThemeData {
+  id: string;
+  userId: string;
+  name: string;
+  description: string;
+  themeColor: string;
+  notesCount: number;
+  workspaceId: string;
+}
+
+type SetSideNotesBook = Dispatch<SetStateAction<SideNote[]>>;
+
+interface ModalProps {
+  onClose: () => void;
+  show: boolean;
+}
+
+const NoteModal = ({ onClose, show }: ModalProps) => {
+  if (!show) return null;
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
+      <div className="mr-5 bg-[#F9F9FA] rounded-lg overflow-hidden shadow-xl transform transition-all w-3/5 p-16">
+        <div className="flex justify-between items-center">
+          <div className={`flex items-center`}>
+            <h3 className="text-3xl font-bold text-[#11181C]">Note</h3>
+            <button
+              className={`${paragraphFont.className} ml-3 border border-[#956E60] flex justify-center items-center px-3 py-1.5 text-sm font-medium rounded font-archivo bg-[#FEF2EE] text-[#956E60]`}
+            >
+              Genesis
+            </button>
+          </div>
+          <button
+            onClick={onClose}
+            className="transition text-gray-400 hover:text-gray-600"
+          >
+            <MdClose size={30} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteModal;
--- app/components/SideNotes/Note/Note.tsx
+++ app/components/SideNotes/Note/Note.tsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 
 import { IoLocate } from "react-icons/io5";
-import { MdOutlineExpandMore } from "react-icons/md";
 import { formatTimestamp } from "@/lib/utils/dateUtils";
 import { MdModeEditOutline } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
 import { Dispatch, SetStateAction } from "react";
 import { handleDelete } from "@/lib/utils/data/sideNotes/book/deleteSideNotesBook";
-import Modal from "../CreateNoteModal";
-import { extractHexColor } from "@/lib/utils/extractHexColor";
+import NoteModal from "./Modal";
 
 interface SideNote {
   id: string;
@@ -53,53 +51,43 @@ const Note = ({ noteData, previousNotes, setNotes, userThemes }: any) => {
   // change formatted date to updated date
 
   return (
-    <button
-      // style={{
-      //   border: `1px solid ${extractHexColor(noteData.theme.themeColor)}`,
-      // }}
-      className="drop-shadow-sm rounded text-black w-full px-4 bg-[#FBFCFD] flex flex-col mb-3"
-    >
-      <div className="flex items-center justify-between w-full">
-        <div className="flex items-center">
-          <div
-            className={`w-3 h-3 ${noteData.theme.themeColor} rounded-sm mr-3`}
-          ></div>
-          <p className="font-semibold">{noteData.title}</p>
+    <>
+      <button
+        onClick={openModal}
+        className="drop-shadow-sm rounded text-black w-full px-4 bg-[#FBFCFD] flex flex-col mb-3"
+      >
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center">
+            <div
+              className={`w-3 h-3 ${noteData.theme.themeColor} rounded-sm mr-3`}
+            ></div>
+            <p className="font-semibold">{noteData.title}</p>
+          </div>
+          <p className="text-[#B5B5B5] text-left font-light my-4">1:2</p>
         </div>
-        <p className="text-[#B5B5B5] text-left font-light my-4">1:2</p>
-      </div>
-      <p>{noteData.description}</p>
-      <div className="flex justify-between items-center w-full">
-        <p className="text-[#B5B5B5] text-xs font-light my-4">
-          {formattedDate}
-        </p>
-        <div className="flex items-center">
-          <button className="mr-2 text-[#B5B5B5]">
-            <IoLocate size={20} />
-          </button>
-          <button onClick={openModal} className="mr-2 text-[#B5B5B5]">
-            <MdModeEditOutline size={20} />
-          </button>
-          <button
-            onClick={() => handleDelete(noteData.id, setNotes)}
-            className="text-[#B5B5B5]"
-          >
-            <MdDelete size={20} />
-          </button>
+        <p>{noteData.description}</p>
+        <div className="flex justify-between items-center w-full">
+          <p className="text-[#B5B5B5] text-xs font-light my-4">
+            {formattedDate}
+          </p>
+          <div className="flex items-center">
+            <button className="mr-2 text-[#B5B5B5]">
+              <IoLocate size={20} />
+            </button>
+            <button onClick={openModal} className="mr-2 text-[#B5B5B5]">
+              <MdModeEditOutline size={20} />
+            </button>
+            <button
+              onClick={() => handleDelete(noteData.id, setNotes)}
+              className="text-[#B5B5B5]"
+            >
+              <MdDelete size={20} />
+            </button>
+          </div>
         </div>
-      </div>
-      <Modal
-        show={showModal}
-        toEdit={true}
-        onClose={closeModal}
-        previousNotes={previousNotes}
-        setNotes={setNotes}
-        prevTitle={noteData.title}
-        prevDescription={noteData.description}
-        prevNoteId={noteData.id}
-        userThemes={userThemes}
-      />
-    </button>
+      </button>
+      <NoteModal show={showModal} onClose={closeModal} />
+    </>
   );
 };
 
